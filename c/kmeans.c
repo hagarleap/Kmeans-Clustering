a634e157f7568882bfc43bec7717b5ea3484d3ff
Gal Kariel <galkariel@localhost.localdomain>
2022-12-02 18:47:51 +0200

    reorder Git

---
+++ c/kmeans.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+
+
+
+int main(int argc, char *argv[])
+{
+    int    k, iter;
+
+    if (argc != 3) {
+        fprintf (stdout, "wrong number of arguments!\n");
+        exit (1);
+    }
+    k = atoi (argv[0]);
+    iter = atoi (argv[1]);
+
+    
+   
+}
