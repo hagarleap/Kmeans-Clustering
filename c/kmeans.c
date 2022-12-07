9562adbeb96bc8712017caa5a4352f3005572b6f
Gal Kariel <galkariel@localhost.localdomain>
2022-12-07 08:41:38 +0200

    Fix the Kmeans file

--- c/kmeans.c
+++ c/kmeans.c
@@ -1,108 +1,81 @@
 # include <stdio.h>
 # include <stdlib.h>
-# include <math.h>
-# include <string.h>
 
+struct cord
+{
+    double value;
+    struct cord *next;
+};
+struct vector
+{
+    struct vector *next;
+    struct cord *cords;
+};
 
 int main(int argc, char *argv[])
 {
-    int    k, iter;
+    FILE *fp;
+    int K;
+    int iter;
+    char *filename;
+    char ch;
 
-    if ((argc > 3) || (argc <= 1)) {
+    if ((argc > 4) || (argc <= 1)) {
         fprintf (stdout, "wrong number of arguments!\n");
         exit (1);
     }
-    
-    if (argc =2) {
-        k = atoi (argv[0]);
+        
+    if (argc == 3) {
+        K = atoi (argv[1]);
         iter = 200;
+        filename = argv[2];
     }
     else{
-        k = atoi (argv[0]);
-        iter = atoi (argv[1]);
+        K = atoi (argv[1]);
+        iter = atoi (argv[2]);
+        filename = argv[3];
     }
+
     
+    fp = fopen(filename, "r");
 
-   
-}
+    struct vector *head_vec, *curr_vec, *next_vec;
+    struct cord *head_cord, *curr_cord, *next_cord;
+    int i, j, rows = 0, cols;
+    double n;
+    char c;
 
+    head_cord = malloc(sizeof(struct cord));
+    curr_cord = head_cord;
+    curr_cord->next = NULL;
 
-  
-    // f = open(input_data, 'r')
-    // vectors = []
-    // vector = f.readline().strip().split(",")
-    // while vector[0] != "":
-    //     vectors.append([float(x) for x in vector])
-    //     vector = f.readline().strip().split(",")
-    // f.close()
-//loading the txt to an array of doubles
-
-int find_array_dimensions(char *filename){
-    int MAX_LINE_LENGTH=100000;
-    int count_line=0;
-    int count_N=0;
-    int res[2];
-    // Open file in read-only mode
-    textfile = fopen(filename, "r");
-    if(textfile == NULL){
-        return 1;
-    }
-
-    //figuring N (len of the vector) 
-    fgets(line, MAX_LINE_LENGTH, textfile);
-    char *token;
-    /* get the first token- first number */
-    token = strtok(line, delimiter);
-    /* walk through other tokens */
-    while( token != NULL ) {
-        count_N++;
-    }
-
-    count_line=1;
-    //figuring and number of lines
-    while(fgets(line, MAX_LINE_LENGTH, textfile)){
-        if (strlen(line)>0){
-            count_line++;
-        }      
-    }
-    fclose(textfile);
-    res[0]=count_line;
-    res[1]=count_N;
-
-    return res;
-}
-    
+    head_vec = malloc(sizeof(struct vector));
+    curr_vec = head_vec;
+    curr_vec->next = NULL;
 
 
-double load_text(char *filename,count_line,count_N){
-    int MAX_LINE_LENGTH=100000;
-    char delimiter= ",";
-    double num; 
-    int i=0, j=0; 
-    // Open file in read-only mode
-    textfile = fopen(filename, "r");
-    double vectors[count_line][count_N];
-    while(fgets(line, MAX_LINE_LENGTH, textfile)){
-        char *token;
-        /* get the first token- first number */
-        token = strtok(line, delimiter);
-        /* walk through other tokens */
-        while( token != NULL ) {
-            if (j<count_N-1){
-                num = atof(token);
-            }
-            else{
-                num_without/n = strtok(token, "\n");
-                num = atof(num_without/n);
-            }
-            vectors[i][j]=num;
-            token = strtok(NULL, delimiter);
-            j++;
+    while (fscanf(fp,"%lf%c", &n, &c) == 2)
+    {
+        printf("Entered n: %lf  c: %c \n", n, c);
+        if (c == '\n')
+        {
+            curr_cord->value = n;
+            curr_vec->cords = head_cord;
+            curr_vec->next = malloc(sizeof(struct vector));
+            curr_vec = curr_vec->next;
+            curr_vec->next = NULL;
+            head_cord = malloc(sizeof(struct cord));
+            curr_cord = head_cord;
+            curr_cord->next = NULL;
+            continue;
         }
-        i++;
+
+        curr_cord->value = n;
+        curr_cord->next = malloc(sizeof(struct cord));
+        curr_cord = curr_cord->next;
+        curr_cord->next = NULL;
     }
-    fclose(textfile);
-    
-    return vectors;
+
+    fclose(fp);
+    return 0;
 }
-    
