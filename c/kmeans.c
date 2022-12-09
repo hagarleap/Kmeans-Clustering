470857136e5ce48026c8970449c6dd9371d19feb
Hagar Lea Paytan <hagarleap@localhost.localdomain>
2022-12-09 14:06:26 +0200

    fixed vector creator, put it in a function 'load_file', attemted to add a counter

--- c/kmeans.c
+++ c/kmeans.c
@@ -1,5 +1,6 @@
 # include <stdio.h>
 # include <stdlib.h>
+#define EPSILON  0.001;
 
 struct cord
 {
@@ -12,34 +13,13 @@ struct vector
     struct cord *cords;
 };
 
-int main(int argc, char *argv[])
-{
+struct vector load_file(char* filename, int *N){
     FILE *fp;
-    int K;
-    int iter;
-    char *filename;
-    char ch;
-
-    if ((argc > 4) || (argc <= 1)) {
-        fprintf (stdout, "wrong number of arguments!\n");
-        exit (1);
-    }
-        
-    if (argc == 3) {
-        K = atoi (argv[1]);
-        iter = 200;
-        filename = argv[2];
-    }
-    else{
-        K = atoi (argv[1]);
-        iter = atoi (argv[2]);
-        filename = argv[3];
-    }
-
+    int count = 0;
     
     fp = fopen(filename, "r");
 
-    struct vector *head_vec, *curr_vec, *next_vec;
+    struct vector *head_vec, *curr_vec, *next_vec, *prev_vec;
     struct cord *head_cord, *curr_cord, *next_cord;
     int i, j, rows = 0, cols;
     double n;
@@ -56,12 +36,15 @@ int main(int argc, char *argv[])
 
     while (fscanf(fp,"%lf%c", &n, &c) == 2)
     {
+        count++;
+        printf("%d", *N);
         printf("Entered n: %lf  c: %c \n", n, c);
         if (c == '\n')
         {
             curr_cord->value = n;
             curr_vec->cords = head_cord;
             curr_vec->next = malloc(sizeof(struct vector));
+            prev_vec = curr_vec;
             curr_vec = curr_vec->next;
             curr_vec->next = NULL;
             head_cord = malloc(sizeof(struct cord));
@@ -75,7 +58,44 @@ int main(int argc, char *argv[])
         curr_cord = curr_cord->next;
         curr_cord->next = NULL;
     }
-
+    prev_vec->next = NULL;
     fclose(fp);
-    return 0;
+    *N = count;
+    return *head_vec;
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    int K;
+    int iter;
+    char *filename;
+    char ch;
+    struct vector *vectors;
+    int *N;
+    int i=0;
+    N=&i;
+
+    if ((argc > 4) || (argc <= 2)) {
+        fprintf (stdout, "wrong number of arguments!\n");
+        exit (1);
+    }
+        
+    if (argc == 3) {
+        K = atoi (argv[1]);
+        iter = 200;
+        filename = argv[2];
+    }
+    else{
+        K = atoi (argv[1]);
+        iter = atoi (argv[2]);
+        filename = argv[3];
+    }
+    
+    *vectors = load_file(filename, N);
+    printf("%d", *N);
+
+    return 1;
+
 }
